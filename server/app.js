d7a720dd7edd6cabcbf3fafade4f79b26241b794
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-11-10 18:56:28 +0530

    connected to port and mongodb

--- server/app.js
+++ server/app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const mongoose = require("mongoose");
+const { MONGOURI } = require("./keys");
 
-app.get("/", (req, res) => {
-  res.send("Hey lets make a instagram clone");
+mongoose.connect(MONGOURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongodb");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("error in connecting mongodb", err);
+});
+
+//77qbVnnhnd5yBmSi
 
 app.listen(PORT, () => {
   console.log("Server is started at", PORT, "CC dug dug dug dug dug.......");
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://naveen:77qbVnnhnd5yBmSi@cluster0.gzgz0.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
