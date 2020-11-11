8d5f17b373e839c383e621a538855f8954cae7fa
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-11-11 17:22:39 +0530

    stories-post model $ route handler done

--- server/app.js
+++ server/app.js
@@ -4,11 +4,7 @@ const PORT = 5000;
 const mongoose = require("mongoose");
 const { MONGOURI } = require("./keys");
 
-require("./models/user");
-
-app.use(express.json());
-app.use(require("./routes/auth"));
-
+//mongodb connect
 mongoose.connect(MONGOURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -20,7 +16,13 @@ mongoose.connection.on("error", (err) => {
   console.log("error in connecting mongodb", err);
 });
 
-//77qbVnnhnd5yBmSi
+//user model
+require("./models/user");
+require("./models/post");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
 
 app.listen(PORT, () => {
   console.log("Server is started at", PORT, "CC dug dug dug dug dug.......");
